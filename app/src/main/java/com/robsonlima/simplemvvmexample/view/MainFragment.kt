07f520bda68685d86f4b0074bc87cbcc6f6501a6
Robson Lima <rsl.robson@gmail.com>
2022-09-20 22:04:44 -0300

    MainFragment class View Binding implementation.

--- app/src/main/java/com/robsonlima/simplemvvmexample/view/MainFragment.kt
+++ app/src/main/java/com/robsonlima/simplemvvmexample/view/MainFragment.kt
@@ -1,7 +1,31 @@
 package com.robsonlima.simplemvvmexample.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.robsonlima.simplemvvmexample.databinding.FragmentMainBinding
 
 class MainFragment: Fragment() {
+    // View Binding with 'fragment_main.xml' so we get reference to xml layout objects easier.
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflating the layout/view
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Invalidate the binding
+        _binding = null
+    }
 }
