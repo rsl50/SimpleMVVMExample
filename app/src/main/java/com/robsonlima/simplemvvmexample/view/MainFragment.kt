5f31e275f3bcee95a7f48f0c49d4b061978c35d3
Robson Lima <rsl.robson@gmail.com>
2022-09-20 22:17:59 -0300

    ViewModel implementation.

--- app/src/main/java/com/robsonlima/simplemvvmexample/view/MainFragment.kt
+++ app/src/main/java/com/robsonlima/simplemvvmexample/view/MainFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.robsonlima.simplemvvmexample.databinding.FragmentMainBinding
+import com.robsonlima.simplemvvmexample.viewmodel.MainViewModel
 
 class MainFragment: Fragment() {
     // View Binding with 'fragment_main.xml' so we get reference to xml layout objects easier.
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +26,22 @@ class MainFragment: Fragment() {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragmentTextUpdateObserver()
+    }
+
+    private fun fragmentTextUpdateObserver() {
+        // Adds a Observer waiting for viewModel to update our UI
+        // updatedText is an alias to help the code be more clear
+        // If data of 'uiTextLiveData' changes, then it is set to 'fragmentTextView' on
+        // fragment_main.xml
+        viewModel.uiTextLiveData.observe(viewLifecycleOwner, Observer { updatedText ->
+            binding.fragmentTextView.text = updatedText
+        })
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/robsonlima/simplemvvmexample/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/robsonlima/simplemvvmexample/viewmodel/MainViewModel.kt
@@ -1,4 +1,11 @@
 package com.robsonlima.simplemvvmexample.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    // Create MutableLiveData which MainFragment can subscribe to
+    // When this data changes, it triggers the UI to do an update
+    val uiTextLiveData = MutableLiveData<String>()
 }
